9913e34877575084731af945d039da9f83d0ca93
Nicolas Hudavert <n.hudavert@oodrive.com>
2020-11-01 22:59:35 +0100

    refactor: localStorage methods stringify parse

--- src/app/shared/services/local-storage.service.ts
+++ src/app/shared/services/local-storage.service.ts
@@ -7,12 +7,26 @@ export class LocalStorageService {
 
   constructor() { }
 
-  get(key: string): any[] | string {
+  get(key: string): string {
     return window.localStorage.getItem(key);
   }
 
+  // get an item when its value is a stringified object
+  getValueParsed(key: string): {} {
+    return JSON.parse(this.get(key));
+  }
+
+  // set item when value is a string
   setItem(key: string, value: string): void {
     window.localStorage.setItem(key, value);
+  }
+
+  /**
+   * if value is an object
+   * then we stringify it, in order to be able to put it in local storage
+   */
+  setItemStringified(key: string, value: {}): void {
+    window.localStorage.setItem(key, JSON.stringify(value));
   }
 
 }
