082577dd240838af71d3c67dab6e51a61f977a3a
Nicolas Hudavert <n.hudavert@oodrive.com>
2020-11-01 22:53:41 +0100

    refactor: create dashboard style

--- src/app/components/create-dashboard/create-dashboard.component.ts
+++ src/app/components/create-dashboard/create-dashboard.component.ts
@@ -1,5 +1,9 @@
+import { LocalStorageService } from 'src/app/shared/services/local-storage.service';
+
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'easy-create-dashboard',
@@ -9,18 +13,56 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 export class CreateDashboardComponent implements OnInit {
 
   addPeopleForm: FormGroup;
-  person1placeholder = 'person #1 name';
   person1name: FormControl;
+  inputs = ['1'];
+  faUserPlus = faUserPlus;
 
   constructor(
     private fb: FormBuilder,
-  ) { }
+    private localStorage: LocalStorageService,
+    private router: Router,
+  ) {
+   }
 
   initForm(): void {
     this.person1name = new FormControl('', Validators.required);
     this.addPeopleForm = this.fb.group({
       person1name : this.person1name,
     });
+  }
+
+  /**
+   * add an input at the bottom of the others
+   * also add its formControl to formGroup
+   */
+  addInputToForm(): void {
+    const personNumber = this.inputs.length + 1;
+    this.inputs.push(`${personNumber}`);
+    this.addPeopleForm.addControl(`person${personNumber}name`, new FormControl('', Validators.required));
+  }
+
+  placeholder(personNumber: number): string {
+    return `person #${personNumber} name...`;
+  }
+
+  save(formData: any): void {
+    const dashboardNumber = this.localStorage.get('numberOfDashboards');
+    /**
+     * When clicking on lets go button,
+     * if there are no dashboards yet in local storage,
+     * set a key to tell the number of dashboards
+     */
+    if (!dashboardNumber) {
+      this.localStorage.setItem('numberOfDashboards', '1');
+      this.localStorage.setItemStringified('peopleList#1', formData);
+    } else {
+      // tslint:disable-next-line: radix
+      this.localStorage.setItem('numberOfDashboards', (parseInt(dashboardNumber) + 1).toString());
+      // tslint:disable-next-line: radix
+      this.localStorage.setItemStringified(`peopleList#${(parseInt(dashboardNumber) + 1)}`, formData);
+    }
+
+    this.router.navigate(['/manage-dashboard']);
   }
 
   ngOnInit() {
