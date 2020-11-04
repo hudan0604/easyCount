ca9a0c68c906aecdfa31afa524c46ac212b02277
Nicolas Hudavert <n.hudavert@oodrive.com>
2020-11-04 22:53:27 +0100

    feat: lateral menu icon in navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
+import { MenuService } from 'src/app/shared/services/menu.service';
+
 import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'easy-navbar',
@@ -7,15 +10,22 @@ import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 })
 export class NavbarComponent {
 
+  faMenu = faEllipsisV;
   @ViewChild('nav', {static: false}) navBar: ElementRef<HTMLElement>;
 
   isScrolled = false;
-  constructor() {}
+  status = false;
+  constructor(private menuService: MenuService) {}
 
   @HostListener('window:scroll', ['$event'])
     handleScroll() {
       const windowScroll = window.pageYOffset;
       this.isScrolled = windowScroll > 80 ? true : false;
-    }
+  }
 
+  toggleMenu(): void {
+    this.status = !this.status;
+    console.log('navbar click on menu logo - this.open = ', this.status);
+    this.menuService.setLateralMenuStatus(this.status);
+  }
 }
