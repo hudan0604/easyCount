e5dea7311936374071ee1322bcf2d7fa82d24e2c
Nicolas Hudavert <n.hudavert@oodrive.com>
2020-11-04 22:41:46 +0100

    feat: lateral menu angular animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { MenuService } from './shared/services/menu.service';
 
 @Component({
   selector: 'easy-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'easyCount';
+  openMenu = false;
+  sub: Subscription;
+
+  constructor(
+    private menuService: MenuService,
+  ) { }
+
+  ngOnInit() {
+    this.menuService.lateralMenuStatus
+      .subscribe(
+        (status: boolean) => {
+        this.openMenu = status;
+        }
+      );
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
 }
