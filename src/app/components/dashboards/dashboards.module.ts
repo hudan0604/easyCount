517ddacdd8dc1e20b2f1716eaa48667624c56e36
Nicolas Hudavert <n.hudavert@oodrive.com>
2020-11-15 21:37:59 +0100

    refactor: delete input component

--- src/app/components/dashboards/dashboards.module.ts
+++ src/app/components/dashboards/dashboards.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
@@ -7,12 +8,11 @@ import { DashboardDetailComponent } from './dashboard-detail/dashboard-detail.co
 import { DashboardHeaderComponent } from './dashboard-header/dashboard-header.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DashboardsLayoutComponent } from './dashboards-layout/dashboards-layout.component';
-import { DashboardsSearchComponent } from './dashboards-search/dashboards-search.component';
 import { DashboardsListComponent } from './list/dashboards-list.component';
+import { ObjectLengthPipe } from './list/object-length.pipe';
 import {
     NoDashboardSelectedComponent
 } from './no-dashboard-selected/no-dashboard-selected.component';
-import { ObjectLengthPipe } from './list/object-length.pipe';
 
 const DASHBOARDS_ROUTES: Routes = [
   {
@@ -37,7 +37,6 @@ const DASHBOARDS_ROUTES: Routes = [
   declarations: [
     DashboardsLayoutComponent,
     DashboardHeaderComponent,
-    DashboardsSearchComponent,
     DashboardComponent,
     DashboardsListComponent,
     DashboardDetailComponent,
@@ -48,6 +47,8 @@ const DASHBOARDS_ROUTES: Routes = [
     CommonModule,
     RouterModule.forChild(DASHBOARDS_ROUTES),
     FontAwesomeModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class DashboardsModule {
