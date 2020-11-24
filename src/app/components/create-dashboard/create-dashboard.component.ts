51d3035fb70d331bc1275a3e1b770970a48f153a
Nicolas Hudavert <hudanico@hotmail.com>
2020-11-24 10:52:06 +0100

    refactor: rename function

--- src/app/components/create-dashboard/create-dashboard.component.ts
+++ src/app/components/create-dashboard/create-dashboard.component.ts
@@ -1,8 +1,8 @@
-import { LocalStorageService } from 'src/app/shared/services/local-storage.service';
+import { DashboardsService } from 'src/app/shared/services/dashboards.service';
+import { ToastService } from 'src/app/shared/services/toast.service';
 
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
 import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -20,13 +20,12 @@ export class CreateDashboardComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
-    private localStorage: LocalStorageService,
-    private router: Router,
+    private dashboardsService: DashboardsService,
+    private toastService: ToastService,
   ) {
    }
 
   initForm(): void {
-    // const activityName = new FormControl('', Validators.required);
       this.addPeopleForm = this.fb.group({
       activityName: this.fb.control('', Validators.required),
       person1name: this.fb.control('', Validators.required),
@@ -51,30 +50,24 @@ export class CreateDashboardComponent implements OnInit {
     return `person${index}name`;
   }
 
-  save(formData: any): void {
-    const dashboardNumberInLocalStorage = this.localStorage.get('numberOfDashboards');
-    // tslint:disable-next-line: radix
-    const dashboardsTotal = (parseInt(dashboardNumberInLocalStorage) + 1).toString();
-    /**
-     * When clicking on lets go button,
-     * if there are no dashboards yet in local storage,
-     * set a key to tell the number of dashboards
-     */
-    if (!dashboardNumberInLocalStorage) {
-      this.localStorage.setItem('numberOfDashboards', '1');
-      this.localStorage.setItemStringified('dashboards', [formData]);
-    } else {
-      // tslint:disable-next-line: radix
-      // tslint:disable-next-line: no-unused-expression
-      // tslint:disable-next-line: radix
-      this.localStorage.setItem('numberOfDashboards', dashboardsTotal);
-      let dashboards = [];
-      dashboards = this.localStorage.getValueParsed('dashboards');
-      dashboards.push(formData);
-      this.localStorage.setItemStringified('dashboards', dashboards);
-    }
-
-    this.router.navigate(['/manage-dashboard']);
+  createNewDashboard(formData: any): void {
+    const date = new Date();
+    // Add date of the day to data that will be sent to Back-end
+    formData.creationDate = date.toDateString();
+    const dataForBackend: any  = {};
+    dataForBackend.activityName = formData.activityName;
+    dataForBackend.creationDate = formData.creationDate;
+    const people: string[] = [];
+    formData = Object.entries(formData)
+      .filter(
+        (key) => key[0] !== 'creationDate' && key[0] !== 'activityName'
+    );
+    formData.map((peopleNameArr) => people.push(peopleNameArr[1]));
+    dataForBackend.people = people;
+    this.dashboardsService.addNewDashboard(dataForBackend).subscribe(
+      (success) => this.toastService.openToast('success', 'Dashboard successfully created !'),
+      (error) => this.toastService.openToast('error', 'Error during dashboard creation')
+    );
   }
 
   ngOnInit() {
