d0ec9d50ef76cf253265c7eb60a08b8546164acf
Nicolas Hudavert <hudanico@hotmail.com>
2020-12-14 17:05:32 +0100

    feat: guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import { CreateDashboardComponent } from './components/create-dashboard/create-d
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login-signIn/login/login.component';
 import { SignInComponent } from './components/login-signIn/sign-in/sign-in.component';
+import { LoggedInGuard } from './shared/guards/logged-in.guard';
+import { NotLoggedInGuard } from './shared/guards/not-logged-in.guard';
 
 const routes: Routes = [
   {
@@ -13,28 +15,43 @@ const routes: Routes = [
     redirectTo: 'login'
   },
   {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
-    path: 'sign-in',
-    component: SignInComponent
+    path: '',
+    // prevent from declaring the guard one each component
+    canActivate: [NotLoggedInGuard],
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'sign-in',
+        component: SignInComponent
+      },
+    ]
   },
   {
     path: 'home',
-    component: HomeComponent
+    component: HomeComponent,
+    canActivate: [LoggedInGuard]
   },
   {
     path: 'dashboards',
     loadChildren: () => import('../app/components/dashboards/dashboards.module').then(m => m.DashboardsModule),
+    canLoad: [LoggedInGuard]
   },
   {
     path: 'create-dashboard',
     component: CreateDashboardComponent,
+    canActivate: [LoggedInGuard]
   },
   {
     path: 'manage-dashboard',
     loadChildren: () => import('../app/components/manage-dashboards/manage-dashboards.module').then(m => m.ManageDashboardsModule),
+    canLoad: [LoggedInGuard]
+  },
+  {
+    path: '**',
+    redirectTo: ''
   }
 ];
 
---
+++ src/app/shared/guards/logged-in.guard.ts
@@ -0,0 +1,37 @@
+import { Observable } from 'rxjs';
+
+import { Injectable } from '@angular/core';
+import {
+    ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment,
+    UrlTree
+} from '@angular/router';
+
+import { UserService } from '../services/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoggedInGuard implements CanActivate, CanLoad {
+
+  constructor(
+    private router: Router,
+    private userservice: UserService
+  ) { }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.userservice.isUserLoggedIn() || this.router.parseUrl('');
+  }
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.userservice.isUserLoggedIn()) {
+      return true
+    } else {
+      this.router.navigate(['']);
+      return false;
+    }
+
+  }
+}
