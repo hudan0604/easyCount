b161f9ca5c321c92d59e294b28cee44f46b2155e
Nicolas Hudavert <hudanico@hotmail.com>
2021-01-03 22:24:03 +0100

    feat: balances component

--- src/app/components/manage-dashboards/balance/balance.component.ts
+++ src/app/components/manage-dashboards/balance/balance.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { ExpenseModel } from 'src/app/shared/models/expense.models';
+import { UserModel } from 'src/app/shared/models/users.models';
+
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'easy-balance',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BalanceComponent implements OnInit {
 
+  @Input() expenses: ExpenseModel[];
+  expensesBalance = {};
+  finalArrayOfBalances = [];
+
   constructor() { }
 
+  calculateEachPersonBalance() {
+    this.expenses.map((expense: ExpenseModel) => {
+      const fullName = `${expense.paiedBy.firstName} ${expense.paiedBy.lastName}`;
+      this.expensesBalance[fullName] = this.expensesBalance[fullName] ? this.expensesBalance[fullName] + expense.amount : expense.amount;
+      expense.forPeople.map((user: UserModel) => {
+        const fullName = `${user.firstName} ${user.lastName}`;
+        this.expensesBalance[fullName] = this.expensesBalance[fullName] ? this.expensesBalance[fullName] - (expense.amount / expense.forPeople.length) : - (expense.amount / expense.forPeople.length);
+      });
+    });
+    // Transform object into array of objects; each obj represents a person with its balance
+    // This will be useful into balance component template to iterate over each array obj
+    Object.keys(this.expensesBalance).map((key) =>
+      this.finalArrayOfBalances.push({
+        personFullName: key,
+        balance: this.expensesBalance[key]
+      }));
+  }
+
+  isBalancePositive(balance: string): boolean {
+    return balance.toString().includes('-') ? false : true;
+  }
+
   ngOnInit() {
+    this.calculateEachPersonBalance();
   }
 
 }
